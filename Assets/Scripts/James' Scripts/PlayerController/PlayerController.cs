2fe071fff9fb1433de9fe84f1566bc0a823ff2e3
jamesrone <32492411+jaamesrone@users.noreply.github.com>
2024-01-29 23:33:54 -0800

    mouse looking (in progress)

--- Assets/Scripts/James' Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/James' Scripts/PlayerController/PlayerController.cs
@@ -1,17 +1,19 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class ShipMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     private Vector2 moveInput;
+    private Vector2 lookInput;
     private Rigidbody rb;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void OnMove(InputValue value)
@@ -19,9 +21,19 @@ public class ShipMovement : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
+    void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
+
     void FixedUpdate()
     {
+        // Move the ship
         Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y) * speed * Time.deltaTime;
         rb.MovePosition(rb.position + movement);
+
+        
+
+        
     }
 }
