78b50b7ffd32e518131a1e9ce83a844cb83c3527
jamesrone <32492411+jaamesrone@users.noreply.github.com>
2024-01-31 00:04:42 -0800

    added player class, player script and shrunk player down for better in game

---
+++ Assets/Scripts/James' Scripts/PlayerController/PlayerClass.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClass : MonoBehaviour
+{
+    public float health;
+    public float defense;
+    public float stamina;
+}
