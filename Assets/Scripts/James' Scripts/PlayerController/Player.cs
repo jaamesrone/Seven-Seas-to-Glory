f20290618321fb39050cac4377ac289929c84540
a13n20 <nhoffmann3@csuchico.edu>
2024-02-05 16:49:54 -0800

    Health Bar Added

--- Assets/Scripts/James' Scripts/PlayerController/Player.cs
+++ Assets/Scripts/James' Scripts/PlayerController/Player.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Player : PlayerClass
 {
+    //Natalie's HealthBar
+    public HealthBar healthBar;
+
+    private void Start()
+    {
+        healthBar.SetMaxHealth(health);
+    }
+
     public void TakeDamage(float damage)
     {
         // player takes dmg
         health -= damage;
+
+        //More of Natalie's Healthbar
+        healthBar.SetHealth(health);
 
         // Check if the pirate's health is below or equal to zero
         if (health <= 0f)
---
+++ Assets/Scripts/Natalie's Scripts/Billboard.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public GameObject healthBar;
+    public Transform cam = null;
+
+    void Start()
+    {
+        GameObject.FindWithTag("MainCamera").GetComponent<Transform>();
+    }
+    void LateUpdate()
+    {
+        healthBar.transform.LookAt(transform.position + cam.forward);
+    }
+}
