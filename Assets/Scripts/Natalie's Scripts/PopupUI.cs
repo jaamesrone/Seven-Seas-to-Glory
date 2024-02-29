cc35c33ed5370a445f4354a089c7f30780a5b79d
a13n20 <143044296+a13n20@users.noreply.github.com>
2024-02-28 20:24:17 -0800

    start if popup ui

---
+++ Assets/Scripts/Natalie's Scripts/PopupUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PopupUI : MonoBehaviour
+{
+    public TextMeshProUGUI popup;
+    public string message = "";
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("PlayerShip"))
+        {
+            popup.text = message;
+        }
+    }
+
+    public void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("PlayerShip"))
+        {
+            popup.text = "";
+        }
+    }
+}
