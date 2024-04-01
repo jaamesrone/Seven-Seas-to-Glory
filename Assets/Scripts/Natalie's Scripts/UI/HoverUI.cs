c9f76cbc277cc7129cd13b93c77695df681f5c38
a13n20 <143044296+a13n20@users.noreply.github.com>
2024-04-01 10:34:29 -0700

    start of shop UI

---
+++ Assets/Scripts/Natalie's Scripts/UI/HoverUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HoverUI : MonoBehaviour
+{
+    public string description;
+    public GameObject descriptionPanel;
+    public TextMeshProUGUI text;
+
+    private void OnMouseEnter()
+    {
+        text.text = description;
+        descriptionPanel.SetActive(true);
+    }
+    private void OnMouseExit()
+    {
+        descriptionPanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Natalie's Scripts/UI/ShopUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopUI : MonoBehaviour
+{
+    public int bulletPrice = 10;
+    public int freesingPrice = 20;
+    public int explodingPrice = 25;
+
+    public Player player;
+}
