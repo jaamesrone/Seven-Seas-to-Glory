9faee8a7fe499b73b312e7b8cfcd6935a4c3295f
a13n20 <143044296+a13n20@users.noreply.github.com>
2024-04-20 14:45:06 -0700

    Multiple cannons

--- Assets/Scripts/James' Scripts/Cannon.cs
+++ Assets/Scripts/James' Scripts/Cannon.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
-    public GameObject firing;
+    public GameObject[] firing;
     public GameObject normalCannonball;
     public GameObject explodeCannonball;
     public GameObject freezeCannonball;
     public GameObject reticle;
     public GameObject reload;
-    public GameObject cannon;
+    public GameObject[] cannons;
 
     public float power;
     public float reloadTime = 3f;
@@ -23,14 +23,16 @@ public class Cannon : MonoBehaviour
     public float increment = 1;
     private float restraint = 0.0f;
 
-    private GameObject currentCannonball;
+    public GameObject[] currentCannonball;
     private bool canFire = true;
     private Coroutine reloadCoroutine;
 
     private void Start()
     {
         reload.SetActive(false);
-        currentCannonball = normalCannonball;
+        currentCannonball[0] = normalCannonball;
+        currentCannonball[1] = normalCannonball;
+        currentCannonball[2] = normalCannonball;
     }
 
     private void Update()
@@ -46,52 +48,87 @@ public class Cannon : MonoBehaviour
 
     void HandleCannonballSwitching()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchCannonball(normalCannonball, 2);
-        if (Input.GetKeyDown(KeyCode.Alpha4) && player.numExplodeCannonballs > 0) SwitchCannonball(explodeCannonball, 3);
-        if (Input.GetKeyDown(KeyCode.Alpha5) && player.numFreezingCannonballs > 0) SwitchCannonball(freezeCannonball, 4);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchCannonball(normalCannonball, 2, -1);
+        if (Input.GetKeyDown(KeyCode.Alpha4) && player.numExplodeCannonballs > 0) SwitchCannonball(explodeCannonball, 3, player.numExplodeCannonballs);
+        if (Input.GetKeyDown(KeyCode.Alpha5) && player.numFreezingCannonballs > 0) SwitchCannonball(freezeCannonball, 4, player.numFreezingCannonballs);
     }
 
-    void SwitchCannonball(GameObject prefab, int inventoryIndex)
+    void SwitchCannonball(GameObject prefab, int inventoryIndex, int count)
     {
-        currentCannonball = prefab;
-        inventoryActive.UpdateActive(inventoryIndex);
+        if (count >= 3)
+        {
+            currentCannonball[0] = prefab;
+            currentCannonball[1] = prefab;
+            currentCannonball[2] = prefab;
+            inventoryActive.UpdateActive(inventoryIndex);
+        }
+        else if (count == 2)
+        {
+            currentCannonball[0] = normalCannonball;
+            currentCannonball[1] = prefab;
+            currentCannonball[2] = prefab;
+            inventoryActive.UpdateActive(inventoryIndex);
+        }
+        else if (count == 1)
+        {
+            currentCannonball[0] = prefab;
+            currentCannonball[1] = normalCannonball;
+            currentCannonball[2] = normalCannonball;
+            inventoryActive.UpdateActive(inventoryIndex);
+        }
+        else
+        {
+            currentCannonball[0] = normalCannonball;
+            currentCannonball[1] = normalCannonball;
+            currentCannonball[2] = normalCannonball;
+            inventoryActive.UpdateActive(2);
+        }
     }
 
     void FireCannonBall()
     {
-        GameObject cannonBall = Instantiate(currentCannonball, firing.transform.position, Quaternion.identity,firing.transform);
-        cannonBall.transform.rotation = firing.transform.rotation;
-        Rigidbody rb = cannonBall.GetComponent<Rigidbody>();
-
-        if (rb != null)
+        for(int i=0; i < firing.Length; i++)
         {
-             rb.AddForce(cannonBall.transform.TransformDirection(Vector3.forward) * power, ForceMode.Impulse);
-            // rb.velocity = cannonBall.transform.TransformDirection(Vector3.forward) * power;
+            GameObject cannonBall = Instantiate(currentCannonball[i], firing[i].transform.position, Quaternion.identity, firing[i].transform);
+            cannonBall.transform.rotation = firing[i].transform.rotation;
+            Rigidbody rb = cannonBall.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddForce(cannonBall.transform.TransformDirection(Vector3.forward) * power, ForceMode.Impulse);
+                // rb.velocity = cannonBall.transform.TransformDirection(Vector3.forward) * power;
+            }
         }
-
-        HandleDecrement();
-
         if (reloadCoroutine != null) StopCoroutine(reloadCoroutine);
         reloadCoroutine = StartCoroutine(ReloadCannon());
+        HandleDecrement();
     }
 
-    private void HandleDecrement()
+    private void HandleDecrement() 
     {
-        if(currentCannonball == explodeCannonball)
+        bool explode = false;
+        bool freeze = false;
+        foreach (GameObject cannonball in currentCannonball)
         {
-            player.numExplodeCannonballs -= 1;
-            if(player.numExplodeCannonballs <= 0)
+            if (cannonball == explodeCannonball)
             {
-                currentCannonball = normalCannonball;
+                player.numExplodeCannonballs -= 1;
+                explode = true;
+            }
+            if (cannonball == freezeCannonball)
+            {
+                player.numFreezingCannonballs -= 1;
+                freeze = true;
+                
             }
         }
-        if(currentCannonball == freezeCannonball)
+        if (explode)
         {
-            player.numFreezingCannonballs -= 1;
-            if (player.numFreezingCannonballs <= 0)
-            {
-                currentCannonball = normalCannonball;
-            }
+            SwitchCannonball(explodeCannonball, 3, player.numExplodeCannonballs);
+        }
+        else if (freeze)
+        {
+            SwitchCannonball(freezeCannonball, 4, player.numFreezingCannonballs);
         }
     }
 
@@ -106,34 +143,37 @@ public class Cannon : MonoBehaviour
             yield return null;
             cooldownTimer -= Time.deltaTime;
         }
-        canFire = true;
         reload.SetActive(false);
+        canFire = true;
     }
 
     void AimCannon()
     {
         //Reversed because cannon is upside down
-        cannon.transform.localRotation = Quaternion.Lerp(Quaternion.Euler(minHeight, 0, 0), Quaternion.Euler(-maxHeight, 0, 0), restraint / increment);
-        if (Input.GetKey(KeyCode.W))
+        foreach (GameObject cannon in cannons)
         {
-            if (restraint < increment)
+            cannon.transform.localRotation = Quaternion.Lerp(Quaternion.Euler(minHeight, 0, 0), Quaternion.Euler(-maxHeight, 0, 0), restraint / increment);
+            if (Input.GetKey(KeyCode.W))
             {
-                restraint += Time.deltaTime;
+                if (restraint < increment)
+                {
+                    restraint += Time.deltaTime;
+                }
+                else
+                {
+                    cannon.transform.localRotation = Quaternion.Euler(-maxHeight, 0, 0);
+                }
             }
-            else
+            if (Input.GetKey(KeyCode.S))
             {
-                cannon.transform.localRotation = Quaternion.Euler(-maxHeight, 0, 0);
-            }
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            if (restraint > 0)
-            {
-                restraint -= Time.deltaTime;
-            }
-            else
-            {
-                cannon.transform.localRotation = Quaternion.Euler(minHeight, 0, 0);
+                if (restraint > 0)
+                {
+                    restraint -= Time.deltaTime;
+                }
+                else
+                {
+                    cannon.transform.localRotation = Quaternion.Euler(minHeight, 0, 0);
+                }
             }
         }
     }
